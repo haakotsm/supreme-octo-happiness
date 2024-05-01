1eaa01f4291c3494a22c5e80ea02665af0a73557
oesohasm <hakon.smorvik@telenor.no>
2024-05-01 18:04:08 +0200

    change to swc compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import react from "@vitejs/plugin-react";
+import swc from "@vitejs/plugin-react-swc";
 import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), AutoImport({ imports: ["vitest"], dts: true })],
+  plugins: [swc(), AutoImport({ imports: ["vitest"], dts: true })],
   test: {
     include: [
       "__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,mjsx,cjsx,tsx}",
