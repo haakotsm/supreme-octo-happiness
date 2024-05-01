b8bfce610113d738ffd26e1763e8b574ea512bd8
oesohasm <hakon.smorvik@telenor.no>
2024-05-01 18:44:28 +0200

    open ai spec

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,6 +2,35 @@ import { HttpResponse, http } from "msw";
 import { weatherData } from "./data";
 
 export const handlers = [
+  /**
+   * A fake API endpoint to get weather data.
+   *
+   * @openapi
+   * /api/weather:
+   *   get:
+   *     summary: Get weather data.
+   *     parameters:
+   *       - name: location
+   *         in: query
+   *         description: Location name to filter by.
+   *         schema:
+   *           type: string
+   *       - name: date
+   *         in: query
+   *         description: Date to filter by.
+   *         schema:
+   *           type: string
+   *           format: date
+   *     responses:
+   *       '200':
+   *         description: Weather data.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 $ref: '#/components/schemas/WeatherData'
+   */
   http.get("/api/weather", ({ request }) => {
     const url = new URL(request.url);
     const location = url.searchParams.get("location");
